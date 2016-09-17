ebc78d65190747dca8494b2d310aeb3d79355f22
Chris Landa <stylesuxx@gmail.com>
2016-09-17 21:02:12 +0200

    Test exception message

--- test/test_browserinfo.py
+++ test/test_browserinfo.py
@@ -1,53 +1,59 @@
 from nose.tools import assert_equal
 
-from browserinfo import recent, popular
+import browserinfo
 
 
 class testBrowserInfo():
     def test_most_recent(self):
-        results = recent(1)
+        results = browserinfo.recent(1)
 
         print results
         assert_equal(len(results), 1)
 
     def test_10_most_recent(self):
-        results = recent(10)
+        results = browserinfo.recent(10)
 
         print results
         assert_equal(len(results), 10)
 
     def test_20_most_recent(self):
-        results = recent(20)
+        results = browserinfo.recent(20)
 
         print results
         assert_equal(len(results), 20)
 
     def test_most_popular(self):
-        results = popular(1)
+        results = browserinfo.popular(1)
 
         print results
         assert_equal(len(results), 1)
 
     def test_10_most_popular(self):
-        results = popular(10)
+        results = browserinfo.popular(10)
 
         print results
         assert_equal(len(results), 10)
 
     def test_20_most_popular(self):
-        results = popular(20)
+        results = browserinfo.popular(20)
 
         print results
         assert_equal(len(results), 20)
 
     def test_negative_amount_sanitization(self):
-        results = popular(-1)
+        results = browserinfo.popular(-1)
 
         print results
         assert_equal(len(results), 0)
 
     def test_positive_amount_sanitization(self):
-        results = popular(100)
+        results = browserinfo.popular(100)
 
         print results
         assert_equal(len(results), 20)
+
+    def test_dom_exception_repo_link(self):
+        err = browserinfo.DOMChanged()
+
+        print err
+        assert browserinfo.__repository__ in err.message
