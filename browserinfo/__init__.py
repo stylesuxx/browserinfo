0591aeee235e2a56cb51a2194be9549d27b42f69
Chris Landa <stylesuxx@gmail.com>
2016-09-17 15:31:12 +0200

    Added docstring

--- browserinfo/__init__.py
+++ browserinfo/__init__.py
@@ -1,3 +1,8 @@
+"""
+Wrapper for `browser-info.net <http://www.browser-info.net/>`_ to
+easily retrieve UA (User Agent) strings.
+"""
+
 from Browserinfo import mostRecent, mostPopular
 
-__version__ = '0.1.2'
+__version__ = '0.1.3'
