b18c1d314b7f343aa1fbe106a3ee5f874def46c8
Chris Landa <stylesuxx@gmail.com>
2016-09-17 17:27:41 +0200

    Clenup

--- browserinfo.py
+++ browserinfo.py
@@ -1,12 +1,21 @@
 from lxml import html
 import urllib
 
+
 baseUrl = 'http://www.browser-info.net/useragents'
 recentXpath = ('(//div[@class="datatable"])[1]//tr/td[2]/a/text()')
 popularXpath = ('(//div[@class="datatable"])[2]//tr/td[2]/a/text()')
 
 
 def mostRecent(amount=20):
+    """Fetches UA strings.
+
+    Retrieves most recent UA strings, up to a maximum of 20.
+
+    Kwargs:
+        amount (int): Amount of UA strings to retrieve.
+    """
+
     f = urllib.urlopen(baseUrl)
     tree = html.fromstring(f.read())
     items = tree.xpath(recentXpath)
@@ -15,6 +24,8 @@ def mostRecent(amount=20):
 
 
 def mostPopular(amount=20):
+    """Docstring for most recent"""
+
     f = urllib.urlopen(baseUrl)
     tree = html.fromstring(f.read())
     items = tree.xpath(popularXpath)
--- browserinfo/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Wrapper for `browser-info.net <http://www.browser-info.net/>`_ to
-easily retrieve UA (User Agent) strings.
-"""
-
-from Browserinfo import mostRecent, mostPopular
-
-__version__ = '0.1.3'
