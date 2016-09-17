9cc480935696a291110427fd6a16ba333478888b
Chris Landa <stylesuxx@gmail.com>
2016-09-17 20:21:24 +0200

    Raise exception if requested amound does not match retrieved amount. It means, that the DOM has changed and is no longer parsable

--- browserinfo.py
+++ browserinfo.py
@@ -5,6 +5,8 @@
 from lxml import html
 import urllib
 
+__version__ = '0.1.4'
+__repository__ = 'https://github.com/stylesuxx/browserinfo'
 
 baseUrl = 'http://www.browser-info.net/useragents'
 recentXpath = ('(//div[@class="datatable"])[1]//tr/td[2]/a/text()')
@@ -12,18 +14,46 @@ popularXpath = ('(//div[@class="datatable"])[2]//tr/td[2]/a/text()')
 limit = 20
 
 
+class BrowserinfoError(Exception):
+    """Base class for exceptions thrown by this library.
+
+        :param message: The error message
+        :type message: str
+    """
+    def __init__(self, message):
+        super(BrowserinfoError, self).__init__(message)
+
+
+class DOMChanged(BrowserinfoError):
+    """Raised when the DOM is not parsable.
+
+    Over time, the page might change and no longer be parsable with the current
+    rule set. In case this happens, this exception is raised with a link to
+    the repository to file an issue.
+    """
+    def __init__(self):
+        message = ('The site structure changed and is no longer parsable. '
+                   'Please file an issue on github: %s' % __repository__)
+        super(DOMChanged, self).__init__(message)
+
+
 def _getResults(amount, xPath):
     amount = _sanitizeAmount(amount)
     f = urllib.urlopen(baseUrl)
     tree = html.fromstring(f.read())
     items = tree.xpath(xPath)
+    items = items[:amount]
 
-    return items[:amount]
+    if len(items) != amount:
+        raise DOMChanged
+
+    return items
 
 
 def _sanitizeAmount(amount):
     if amount < 0:
         return 0
+
     if amount > limit:
         return limit
 
@@ -37,14 +67,18 @@ def popular(amount=20):
 
     :param amount: The amount of UA strings to retrieve
     :type amount: int
+
     :returns: List of UA strings
     :rtype: list
+
+    :raises DOMChanged: When retrieved amount does not match requested amount
 
     :Example:
 
     >>> import browserinfo
     >>> browserinfo.popular(1)
     ['Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; FSL 7.0.6.01001)']
+
 
     """
 
@@ -58,8 +92,11 @@ def recent(amount=20):
 
     :param amount: The amount of UA strings to retrieve
     :type amount: int
+
     :returns: List of UA strings
     :rtype: list
+
+    :raises DOMChanged: When retrieved amount does not match requested amount
 
     :Example:
 
