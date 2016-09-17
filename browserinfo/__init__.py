488d23743a65788a534fa1846c3a0ac57473b9da
Chris Landa <stylesuxx@gmail.com>
2016-09-17 21:42:54 +0200

    Import from file

---
+++ browserinfo/__init__.py
@@ -0,0 +1,4 @@
+from .browserinfo import (recent, popular, DOMChanged, BrowserinfoError,
+                          __repository__, __version__)
+
+__all__ = ['popular', 'recent', 'BrowserinfoError', 'DOMChanged']
